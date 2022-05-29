66c3bab16b4989ddf62e35ab1a144b227bb56e70
zdurbin <dur15014@byui.edu>
2022-05-28 22:15:35 -0700

    Validation working

--- routes/todo.js
+++ routes/todo.js
@@ -30,7 +30,35 @@ router.post('/', taskValidation, (req, res) => {
     }
   });
 
-router.put('/:id', taskValidation, (req, res)=> {});
+router.put('/:id', taskValidation, (req, res)=> {
+    const taskId = new ObjectId(req.params.id);
+  // be aware of updateOne if you only want to update specific fields
+  const task = {
+    taskName: req.body.taskName,
+    startDate: req.body.startDate,
+    dueDate: req.body.dueDate,
+    taskPriority: req.body.taskPriority
+
+  };
+
+  const _result = results(req);
+    if (!_result.isEmpty()) {
+        return res.status(500).json({ errors: _result.array() })
+    };
+
+    const response = await mongodb
+    .getDb()
+    .db()
+    .collection('Todo')
+    .replaceOne({ _id: taskId }, task);
+  console.log(response);
+  if (response.modifiedCount > 0) {
+    res.status(204).send();
+  } else {
+    res.status(500).json(response.error || 'Some error occurred while updating the task.');
+  }
+
+});
 
 router.delete('/:id', todoController.deleteTask);
 
