5f11d486b3ea848c4e2a31bcc0702457ca17dd14
zdurbin <dur15014@byui.edu>
2022-07-13 19:57:30 -0700

    Validation update

--- routes/validation.js
+++ routes/validation.js
@@ -1,10 +1,13 @@
 const { check, validationResult} = require('express-validator');
 
 exports.taskValidation = [
-    check('taskName', 'Task name is required').not().isEmpty(),
-    check('startDate', 'Start date is required').not().isEmpty(),
-    check('dueDate', 'completion deadline date is required').not().isEmpty(),
-    check('taskPriority', 'Task importances is required').not().isEmpty()
+    check('Name', 'Task name is required').not().isEmpty(),
+    check('createdDate', 'Start date is required').not().isEmpty(),
+    check('due', 'completion deadline date is required').not().isEmpty(),
+    check('priorityLevel', 'Task importances is required').not().isEmpty(),
+    check('creator', 'creators name is required').not().isEmpty(),
+    check('responsable', 'Task responsablitly is required').not().isEmpty(),
+    check('discription', 'Task description is required').not().isEmpty()
 ];
 
 exports.results = validationResult;
